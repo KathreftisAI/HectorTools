b3ddf8b164a76082c8505d7005ffe8b519ae9d0f
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2018-06-08 06:54:49 +0700

    Removed flag variables

--- utilgen.go
+++ utilgen.go
@@ -8,28 +8,18 @@ import(
 	"encoding/json"
 	"bytes"
 	"strings"
-	"flag"
 	"github.com/spf13/viper"
 )
-/*
-func main(){
-	loadConfiguration()
-	fmt.Println("meow")
-	err := dbm.StartSchemaMapper(Conf.CassNConf)
-
-	if err != nil{
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-}
-*/
 
 type FileConfiguration struct{
 	Host []string // Required
 	Username string // Required
 	Password string // Required
 	Keyspace string
-
+	AppJSONPath string
+	AvroSchemaPath string
+	CassQueryPath string
+	HiveSchemaPath string
 	// Optional
 	NumConnectionsPerHost int
 	ConnectionTimeout int
@@ -62,48 +52,14 @@ type Field struct {
 }
 
 
-var Conf map[string]interface{}
 
 var ConfFile FileConfiguration
 
-func readFlags(){
-	var configFilePath,pwdCass, hostCass, userCass,ksCass,appJsonOut,avScOut,cassQueryOut,hiveScOut string
-	flag.StringVar(&configFilePath,"config_file","","Give fully qualified path of configuration file")
-	flag.StringVar(&hostCass,"host","","Give the hostname with port for cassandra")
-	flag.StringVar(&userCass,"username","","Give the cassandra username")
-	flag.StringVar(&pwdCass,"password","","Give the cassandra password")
-	flag.StringVar(&ksCass,"keyspace","","Define the Cassandra Keyspace to use")
-	flag.StringVar(&appJsonOut,"app_json_out","","Give the output file for storing app json")
-	flag.StringVar(&avScOut,"av_sc_out","","Give the output file for storing Avro Schema")
-	flag.StringVar(&cassQueryOut,"cass_query_out","","Give the output file for storing Cassandra Query")
-	flag.StringVar(&hiveScOut,"hive_sc_out","","Give the output file for storing Hive Schema")
-
-	setConfValue(configFilePath,"config_file")
-	setConfValue(hostCass,"cass_host")
-	setConfValue(userCass,"cass_user")
-	setConfValue(pwdCass,"cass_passwd")
-	setConfValue(ksCass,"cass_keyspace")
-	setConfValue(appJsonOut,"app_json_out")
-	setConfValue(avScOut,"av_sc_out")
-	setConfValue(cassQueryOut,"cass_query_out")
-	setConfValue(hiveScOut,"hive_sc_out")
-
-}
-
-func setConfValue(value string, key string) {
-	if value != ""{
-		Conf[key] = value
-	}
-}
 
 func loadConfiguration(){
 
-	if Conf["cass_host"] == nil || Conf["cass_user"] == nil || Conf["cass_passwd"] == nil || Conf["cass_keyspace"] == nil{
-		if Conf["config_file"] == nil{
-			Conf["config_file"] = "/etc/stream/util.toml"
-		}
 
-		viper.SetConfigFile(Conf["config_file"].(string))
+		viper.SetConfigFile("/etc/stream/util.toml")
 		viper.SetConfigType("toml")
 		verr := viper.ReadInConfig()
 
@@ -117,89 +73,63 @@ func loadConfiguration(){
 			fmt.Println("There was an error reading in configuration. Error : ", verr.Error())
 		}
 
-		if ConfFile.Host != nil && Conf["cass_host"] == nil{
-			Conf["cass_host"] = ConfFile.Host
-		}
-
-		if ConfFile.Host != nil && Conf["cass_user"] == nil{
-			Conf["cass_user"] = ConfFile.Username
-		}
-
-		if ConfFile.Host != nil && Conf["cass_passwd"] == nil{
-			Conf["cass_passwd"] = ConfFile.Password
-		}
-
-		if ConfFile.Host != nil && Conf["cass_keyspace"] == nil{
-			Conf["cass_keyspace"] = ConfFile.Keyspace
-		}
-
-	}
 }
 
 
 func checkConfiguration(){
-	if Conf["cass_host"] == nil{
+	if ConfFile.Host == nil{
 		fmt.Println("No hosts defined, Please check your configuration file or pass it as -host")
 		os.Exit(1)
 	}
 
-	if Conf["cass_user"] == nil{
+	if ConfFile.Username == ""{
 		fmt.Println("No user defined, Please check your configuration file or pass it as -username")
 		os.Exit(1)
 	}
 
-	if Conf["cass_passwd"] == nil{
+	if ConfFile.Password == ""{
 		fmt.Println("No password defined, Please check your configuration file or pass it as -password")
 		os.Exit(1)
 	}
 
-	if Conf["cass_keyspace"] == nil{
-		fmt.Println("No keyspace defined, Please check your configuration file or pass it as -keyspace")
+	if ConfFile.Keyspace == ""{
+		fmt.Println("No keyspace defined, Please check your configuration file")
 		os.Exit(1)
 	}
 
-	if Conf["app_json_out"] == nil{
-		fmt.Println("No application json output file defined, Please pass it as -app_json_out")
+	if ConfFile.AppJSONPath == ""{
+		fmt.Println("No Application Json Output file path defined, Please check your configuration file")
 		os.Exit(1)
 	}
 
-	if Conf["av_sc_out"] == nil{
-		fmt.Println("No avro schema output file defined, Please pass it as -av_sc_out")
+	if ConfFile.AvroSchemaPath == ""{
+		fmt.Println("No Avro Schema Output file path defined, Please check your configuration file")
 		os.Exit(1)
 	}
 
-	if Conf["cass_query_out"] == nil{
-		fmt.Println("No Cassandra Query output file defined, Please pass it as -cass_query_out")
+	if ConfFile.CassQueryPath == ""{
+		fmt.Println("No Cassandra Query Output file path defined, Please check your configuration file")
 		os.Exit(1)
 	}
 
-	if Conf["hive_sc_out"] == nil{
-		fmt.Println("No Hive Schema Output file defined, Please pass it as -hive_sc_out")
+	if ConfFile.HiveSchemaPath == ""{
+		fmt.Println("No Hive Schema Output file defined, Please check your configuration")
 	}
 }
 
 
 func main()  {
 
-	Conf = make(map[string]interface{})
-
-	readFlags()
-
 	loadConfiguration()
 
 	checkConfiguration()
 
-	fmt.Println(Conf["cass_keyspace"].(string)+"ks")
-	fmt.Println(Conf["cass_passwd"].(string)+"pass")
-	fmt.Println(Conf["cass_user"].(string)+"user")
-	fmt.Println(Conf["cass_host"].(string)+"host")
-
 
 	err := dbm.StartSchemaMapper(dbm.CassandraConfig{
-		Keyspace: Conf["cass_keyspace"].(string),
-		Username: Conf["cass_user"].(string),
-		Password: Conf["cass_passwd"].(string),
-		Host: []string{Conf["cass_host"].(string)}})
+		Keyspace: ConfFile.Keyspace,
+		Username: ConfFile.Username,
+		Password: ConfFile.Password,
+		Host: ConfFile.Host})
 
 	if err != nil {
 		fmt.Println(err)
