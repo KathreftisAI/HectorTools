6b5bb399ba72342b3cecafdccc7072cb459381ce
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2018-06-08 09:08:36 +0700

    Added a binary and updated toml. Now also generates Cass Queries

--- utilgen.go
+++ utilgen.go
@@ -144,11 +144,65 @@ func main()  {
 
 
 	MakeApi()
+
 	err = writeAPIJson()
 	if err != nil{
 		fmt.Println("Error writing App JSON file ",err.Error())
 	}
 
+	makeCassQuery()
+
+	err = writeCassQuery()
+
+	if err != nil{
+		fmt.Println("Error writing Cassandra Query file",err.Error())
+	}
+}
+
+
+func writeCassQuery() (err error){
+
+	for key, value := range CassQueries {
+		f,err := os.OpenFile(ConfFile.CassQueryPath+"/"+time.Now().Format("20060102150405")+"-"+key+".cql",os.O_CREATE|os.O_RDWR,0664)
+
+		if err != nil{
+			return err
+		}
+
+		_, err = f.Write([]byte(value))
+
+		if err != nil{
+			return err
+		}
+
+	}
+
+	return
+}
+
+
+var CassQueries map[string]string
+
+func makeCassQuery() {
+	CassQueries = make(map[string]string)
+	for key, value := range dbm.TableArr {
+		var tempData string
+		tempData = "SELECT "
+		for _, value1 := range value.Columns {
+			str1 := value1.Column_name
+			if value1.Datatype.String() == "Timestamp"{
+				str1 = "blogAsBigInt(timestampAsBlob("+str1+")) as "+str1
+			}
+			tempData = tempData+str1+", "
+		}
+
+		runeVal := []rune(tempData)
+
+		tempData = string(runeVal[:len(runeVal)-1])
+		fmt.Println(tempData)
+		tempData = tempData+" FROM "+value.Keyspace_name+"."+key+" WHERE "+value.PartitionColumns[0].Column_name+" IN (?)"
+		CassQueries[key] = tempData
+	}
 }
 
 
